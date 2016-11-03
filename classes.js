226ae04a3e44008c77953daa144388c10f900644
Thomas <terickson@spotxchange.com>
2016-11-03 14:04:39 -0600

    added some methods

--- classes.js
+++ classes.js
@@ -5,10 +5,25 @@ class Monepok{
     this.type = type;
     this.battleStats = battleStats;
   }
+  takeDamage(points){
+    return(this.battleStats['maxHp'] - this.battleStats['attack']);
+  }
+  isFainted(hp){
+    if(hp === 0)
+      return true;
+    else
+      return false;
+  }
 };
 class Shield{
   constructor(strength){
     this.strength = strength;
+  }
+  applyDefense(attackPoints){
+    console.log(attackPoints *= this.strength);
+  }
+  withString(){
+    console.log('with Shield');
   }
 };
 class Weapon{
@@ -16,6 +31,18 @@ class Weapon{
     this.name = name;
     this.strength = strength;
   }
+  applyAttack(attackPoints){
+    return(this.strength * attackPoints);
+  }
+  withString(){
+    return this.name;
+  }
 };
-var wilbur = new Monepok('wilbur','earth',{maxHp: 100, attack: 'ferocious'});
+var wilbur = new Monepok('wilbur','earth',{maxHp: 100, attack: 40});
+console.log(wilbur.takeDamage());
+var shield1 = new Shield(.75);
+shield1.applyDefense(50);
+shield1.withString();
 console.log(wilbur.battleStats);
+var cheetah = new Weapon('ultimate',35);
+cheetah.withString();
